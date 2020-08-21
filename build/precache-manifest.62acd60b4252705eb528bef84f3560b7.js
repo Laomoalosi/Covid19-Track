0fc164718960988f56ae74f87c7809a454ee7a8a
laonemoalosi <laomoalosi@protonmail.com>
2020-08-21 03:21:17 +0200

    first commit

---
+++ build/precache-manifest.62acd60b4252705eb528bef84f3560b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ba04d1425f69b334291e11c72f7a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "12b61113273320497883",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b61113273320497883",
+    "url": "/static/js/main.dd5d3df5.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
